9011752d7150d099c278047962aa02e8d9d85c41
kk.rathore@gmail.com <kk.rathore@gmail.com>
2022-10-29 00:26:38 -0500

    adding app

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello wporld mr. node js \n");
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
