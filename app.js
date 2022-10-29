24fd34225fd54158bc9f0554a87f0ca94abf5c52
kk.rathore@gmail.com <kk.rathore@gmail.com>
2022-10-29 01:33:20 -0500

    version updgrage

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 app = express();
 
+var count = 0;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+    console.log('new request is received on:' + count++ + "::" + new Date());
+    res.send('Hello World ! version 2 deployed\n');
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+    console.log('Example app listening on port 8080!');
 });
 
